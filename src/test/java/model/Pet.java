4e90425f7c776233dfa54ac5c42d3d51c95c33b7
Maxim Fedotenko <max.fedotenko2000@gmail.com>
2025-12-25 00:49:04 +0300

    add POM

--- src/test/java/model/Pet.java
+++ src/test/java/model/Pet.java
@@ -1,8 +1,10 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Pet {
     @JsonProperty("id")
     public long id;
--- src/test/java/pet/PetApiTest.java
+++ src/test/java/pet/PetApiTest.java
@@ -2,25 +2,24 @@ package pet;
 
 import base.TestBase;
 import io.qameta.allure.Description;
-import io.restassured.http.ContentType;
 import io.qameta.allure.junit5.AllureJunit5;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import pages.PetApiPage;
+import model.Pet;
 import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-
-import model.Pet;
 
 @ExtendWith(AllureJunit5.class)
 public class PetApiTest extends TestBase {
+    private final PetApiPage petApi = new PetApiPage();
 
     @Test
-    @Description("Create and get pet test")
-    void createAndGetPet() {
+    @Description("CRUD api test")
+    void CRUDTest() {
         long petId = ThreadLocalRandom.current().nextLong(1, 10000); // Безопасный диапазон
 
         Pet pet = new Pet();
@@ -29,34 +28,24 @@ public class PetApiTest extends TestBase {
         pet.status = "available";
         pet.photoUrls = Collections.singletonList("https://example.com/photo.jpg");
 
-        // Создание
-        given()
-                .contentType(ContentType.JSON)
-                .body(pet)
-                .when()
-                .post("/pet")
-                .then()
-                .statusCode(200)
-                .body("id", equalTo((int) petId))
-                .body("name", equalTo("PetTest1"))
-                .body("status", equalTo("available"));
-        System.out.println("УСПЕШНО создали: ");
-        System.out.println("ID: " + petId);
-        System.out.println("Name: " + pet.name);
-        System.out.println("Status: " + pet.status);
-        // Получение
-        given()
-                .pathParam("petId", petId)
-                .when()
-                .get("/pet/{petId}")
-                .then()
-                .statusCode(200)
-                .body("id", equalTo((int) petId))
-                .body("name", equalTo("PetTest1"))
-                .body("status", equalTo("available"));
-        System.out.println("УСПЕШНО получили: ");
-        System.out.println("ID: " + petId);
-        System.out.println("Name: " + pet.name);
-        System.out.println("Status: " + pet.status);
+        // Post
+        Pet created = petApi.createPet(pet);
+        // Get
+        Pet fetched = petApi.getPet(created.id);
+        // Put, обновили имя и photoUrls
+        fetched.name = "PetTestUpdate";
+        fetched.photoUrls = Collections.singletonList("https://example.com/photo2.jpg");
+        Pet updated = petApi.updatePet(fetched);
+        //Get после Put
+        Pet featchedAfterUpdate = petApi.getPet(updated.id);
+        // Удаление
+        petApi.deletePet(updated.id);
+
+        assertEquals("PetTest1", created.name);
+        assertEquals(created.id, fetched.id);
+        assertEquals("PetTestUpdate", updated.name);
+        assertEquals("https://example.com/photo2.jpg", updated.photoUrls.get(0));
+        assertEquals(updated.name, featchedAfterUpdate.name);
+        assertEquals(updated.photoUrls, featchedAfterUpdate.photoUrls);
     }
 }
